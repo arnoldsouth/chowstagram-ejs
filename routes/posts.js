6da2d2c097c7dc72e8f7dcc687739da4ed654db1
arnoldsouth <arnoldsouth@gmail.com>
2022-10-03 20:20:39 -0700

    Update feed and post ejs

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
+// http://localhost:3210/post/6327c7e5e9c729f8b549cb8a
 router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+// express is used to build out our API
 const express = require("express");
 const app = express();
+// helps us talk to our mongodb db
 const mongoose = require("mongoose");
+// for authentication; uses different strategies for different types of logins (fb, twitter, google, etc)
 const passport = require("passport");
+// allows users to stay logged in as they move across our app. uses cookies, stored on our client. sees who is logged in, allowing us to build out our pages
 const session = require("express-session");
+// stores our actual session in mongodb, keeps us logged in when we leave the app
+// connect-mongo = method; session is argument
 const MongoStore = require("connect-mongo")(session);
+// browser only goes GET and POST, so we use this to override those methods to PUT and DELETE
 const methodOverride = require("method-override");
+// for notifications (wrong password, etc)
 const flash = require("express-flash");
+// shows our logs in terminal
 const logger = require("morgan");
 const connectDB = require("./config/database");
+
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const commentRoutes = require("./routes/comments");
@@ -53,15 +63,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Use flash messages for errors, info, ect...
+// Use flash messages for errors, info, ect...
 app.use(flash());
 
-//Setup Routes For Which The Server Is Listening
+// Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 app.use("/comment", commentRoutes);
 
-//Server Running
+// Server Running
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
